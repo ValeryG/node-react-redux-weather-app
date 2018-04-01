8b6c37b25ce9fcdc06f4430e1f06750ec1d8338c
Takeru Komiya <tkomiya24@users.noreply.github.com>
2018-04-01 12:40:13 -0700

    Retrieve weather information in Fahrenheit (#18)

--- app/api/index.js
+++ app/api/index.js
@@ -2,10 +2,11 @@ const RestApi = require('./restApi');
 const logger = require('../../logger');
 
 const API_ADDRESS = 'api.openweathermap.org/data/2.5/weather';
+const UNIT = 'imperial';
 
 module.exports.getForCity = function(city) {
   return new Promise((resolve, reject) => {
-    RestApi.getJson(API_ADDRESS, { q: city }).then(response => {
+    RestApi.getJson(API_ADDRESS, { q: city, units: UNIT }).then(response => {
       resolve(response);
     }, error => {
       logger.warn(`Error of status ${error.status} received for weather info`,
